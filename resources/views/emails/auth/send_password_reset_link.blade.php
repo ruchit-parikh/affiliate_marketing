30c65c292c171e3225edf1e014d695af38abb43b
Ruchit Parikh <ruchitparikh17@gmail.com>
2020-03-25 23:30:03 +0530

    small message added on email

--- resources/views/emails/auth/send_password_reset_link.blade.php
+++ resources/views/emails/auth/send_password_reset_link.blade.php
@@ -1,5 +1,6 @@
 @component('mail::message')
 
+<span style="text-align: center">You can reset your password by clicking on below button!</span><br><br>
 @component('mail::button', ['url' => route('v1.reset_password', ['token' => $token])])
 {{ __('auth.reset_password_button') }}
 @endcomponent
